53fef0dcbdedb530a875bc92d525fd6bab621b16
Spoiled Unknown <shanjusamsong254@gmail.com>
2023-12-01 22:58:47 +0530

    Completed all the description, and added correct image for newtya

--- src/utils/mobile-navigation.js
+++ src/utils/mobile-navigation.js
@@ -1,32 +1,38 @@
-function MobileNavigation()
-{
-    const headerBtn = document.querySelector('.header__bars')
-    const mobileNav = document.querySelector('.mobile-nav')
-    const mobileLinks = document.querySelectorAll('.mobile-nav__link')
+function MobileNavigation() {
+    const headerBtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
 
-    let isMobileMenuOpen = false
+    let isMobileMenuOpen = false;
 
     headerBtn.addEventListener('click', () => {
-        isMobileMenuOpen = !isMobileMenuOpen
+        isMobileMenuOpen = !isMobileMenuOpen;
+
         if (isMobileMenuOpen) {
-            mobileNav.style.display = 'flex'
+            mobileNav.style.display = 'flex';
             document.body.style.overflowY = 'hidden';
+
+            // Trigger a reflow before changing the opacity for the fade-in effect
+            void mobileNav.offsetWidth;
+            mobileNav.style.opacity = '1'; // Add fade-in effect
+        } else {
+            mobileNav.style.opacity = '0'; // Set opacity to 0 for fade-out effect
         }
-        else
-        {
-            mobileNav.style.display = 'none'
+    });
+
+    mobileNav.addEventListener('transitionend', () => {
+        if (!isMobileMenuOpen) {
+            mobileNav.style.display = 'none';
             document.body.style.overflowY = 'auto';
         }
-    })
+    });
 
     mobileLinks.forEach(link => {
         link.addEventListener('click', () => {
-            isMobileMenuOpen = false
-            mobileNav.style.display = 'none'
-            document.body.style.overflowY = 'auto';
-        })
-    } )
-    
+            isMobileMenuOpen = false;
+            mobileNav.style.opacity = '0'; // Set opacity to 0 for fade-out effect
+        });
+    });
 }
 
-export default MobileNavigation
+export default MobileNavigation;
