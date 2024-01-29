dcd0a2561dbcc35766ed717a69c27356d9c04eea
Spoiled Unknown <shanjusamsong254@gmail.com>
2024-01-29 16:21:20 +0530

    Removed debug codes for deploying

--- src/utils/footer.js
+++ src/utils/footer.js
@@ -1,6 +1,6 @@
 function FooterText()
 {
-    document.querySelector('.footer__title').innerHTML = "Copyright © " + (new Date().getFullYear()) + " Spoiled Unknown. All rights reserved." + "<br /> This website is made using <strong>HTML, JS and CSS</strong> || <a>Version 1.5.1</a>"
+    document.querySelector('.footer__title').innerHTML = "Copyright © " + (new Date().getFullYear()) + " Spoiled Unknown. All rights reserved." + "<br /> This website is made using <strong>HTML, JS and CSS</strong> || <a>Version 1.5.2</a>"
 }
 
 export default FooterText
