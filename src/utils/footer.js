1416a299fb6c283796778cd91a721199d955b4f0
Spoiled Unknown <shanjusamsong254@gmail.com>
2024-01-29 16:28:47 +0530

    Deploying version 1.5.2

--- src/utils/footer.js
+++
@@ -1,6 +0,0 @@
-function FooterText()
-{
-    document.querySelector('.footer__title').innerHTML = "Copyright © " + (new Date().getFullYear()) + " Spoiled Unknown. All rights reserved." + "<br /> This website is made using <strong>HTML, JS and CSS</strong> || <a>Version 1.5.2</a>"
-}
-
-export default FooterText
