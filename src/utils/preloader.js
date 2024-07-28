a52b3ad9acfc2aad05c298d565d03b10a0af0bff
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2024-07-28 10:13:13 +0530

    Update preloader.js

--- src/utils/preloader.js
+++ src/utils/preloader.js
@@ -1,12 +1,9 @@
 export default function HandlePreloading() {
     const preloader = document.querySelector(".preloader");
     const form = document.querySelector(".contact__form");
-    const sleep = (ms = 3000) => new Promise((r) => setTimeout(r, ms));
     document.body.style.overflowY = "hidden";
 
     window.addEventListener("load", async () => {
-        await sleep();
-
         if (form !== null) form.reset();
         preloader.style.display = "none";
         document.body.style.overflowY = "auto";
