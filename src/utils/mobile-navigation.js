6b276eba8afc5338294047ecbc6f6a5a10da9131
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2024-07-29 14:21:13 +0530

    did some "stuff".

--- src/utils/mobile-navigation.js
+++ src/utils/mobile-navigation.js
@@ -1,34 +1,34 @@
 export default function MobileNavigation() {
-    const headerBtn = document.querySelector(".header__bars");
-    const mobileNav = document.querySelector(".mobile-nav");
-    const mobileLinks = document.querySelectorAll(".mobile-nav__link");
+  const headerBtn = document.querySelector(".header__bars");
+  const mobileNav = document.querySelector(".mobile-nav");
+  const mobileLinks = document.querySelectorAll(".mobile-nav__link");
 
-    let isMobileMenuOpen = false;
+  let isMobileMenuOpen = false;
 
-    headerBtn.addEventListener("click", () => {
-        isMobileMenuOpen = !isMobileMenuOpen;
+  headerBtn.addEventListener("click", () => {
+    isMobileMenuOpen = !isMobileMenuOpen;
 
-        if (isMobileMenuOpen) {
-            mobileNav.style.display = "flex";
-            document.body.style.overflowY = "hidden";
-            void mobileNav.offsetWidth;
-            mobileNav.style.opacity = "1";
-        } else {
-            mobileNav.style.opacity = "0";
-        }
+    if (isMobileMenuOpen) {
+      mobileNav.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+      void mobileNav.offsetWidth;
+      mobileNav.style.opacity = "1";
+    } else {
+      mobileNav.style.opacity = "0";
+    }
+  });
+
+  mobileNav.addEventListener("transitionend", () => {
+    if (!isMobileMenuOpen) {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+  });
+
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      isMobileMenuOpen = false;
+      mobileNav.style.opacity = "0";
     });
-
-    mobileNav.addEventListener("transitionend", () => {
-        if (!isMobileMenuOpen) {
-            mobileNav.style.display = "none";
-            document.body.style.overflowY = "auto";
-        }
-    });
-
-    mobileLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            isMobileMenuOpen = false;
-            mobileNav.style.opacity = "0";
-        });
-    });
+  });
 }
