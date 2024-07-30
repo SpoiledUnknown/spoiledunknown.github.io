e0ee0e01bc6b2d7dfacf7d970472beca7bdbd8c3
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2024-07-30 20:01:28 +0530

    minor improvements.

--- src/utils/footer.js
+++ src/utils/footer.js
@@ -1,6 +1,7 @@
-const version = "1.7.1";
+const version = "0.1.1";
 
 export default function FooterText() {
-    document.querySelector(".footer__title").innerHTML =
-        `Copyright © ${new Date().getFullYear()} Spoiled Unknown. All rights reserved. <br /> This website is made using <strong>HTML, JS and CSS</strong> || <a> ${version} </a>`;
+  document.querySelector(
+    ".footer__title"
+  ).innerHTML = `Copyright © ${new Date().getFullYear()} Spoiled Unknown. All rights reserved. <br /> This website is made using <strong>HTML, JS and CSS</strong> || <a> ${version} </a>`;
 }
