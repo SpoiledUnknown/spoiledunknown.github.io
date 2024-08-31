ae283fdc3a2695420ae1bf0ae749647167b530d5
Spoiled Unknown <58999771+SpoiledUnknown@users.noreply.github.com>
2024-08-31 16:47:52 +0530

    minor update
    changed the images for XtremeFPS and Newtya
    changed the pfp
    fixed my existenstial crisis (thanks to gyanology)
    
    other minor improvements.

--- src/utils/footer.js
+++ src/utils/footer.js
@@ -1,7 +1,7 @@
-const version = "0.1.1";
+const version = "0.1.2";
 
 export default function FooterText() {
   document.querySelector(
     ".footer__title"
-  ).innerHTML = `Copyright © ${new Date().getFullYear()} Spoiled Unknown. All rights reserved. <br /> This website is made using <strong>HTML, JS and CSS</strong> || <a> ${version} </a>`;
+  ).innerHTML = `Copyright © ${new Date().getFullYear()} Spoiled Unknown. All rights reserved. || <a> V${version} </a>`;
 }
